a41271c36d92f6fcbe67ead7fb678410f8e3ce5f
thomas <thomas11950@gmail.com>
2020-11-16 14:35:32 -0800

    add logcat debug again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Multithreads/MagFlickerController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Multithreads/MagFlickerController.java
@@ -36,26 +36,27 @@ public class MagFlickerController extends Thread{
         while(!parentOP.teleopStopped){
             if(shootRingRequested){
                 numButtonPresses++;
+                RobotLog.dd(TAG,numButtonPresses+" press checkpoint 1: " + hardware.time.milliseconds());
                 hardware.mag.updateStateAndSetPosition();
-                RobotLog.dd(TAG,numButtonPresses+" press checkpoint 1: " + hardware.mag.currentState.toString());
+                RobotLog.dd(TAG,numButtonPresses+" press checkpoint 2: " + hardware.mag.currentState.toString());
                 if(firstButtonPress){
                     hardware.mag.currentState = Mag.State.TOP;
                     firstButtonPress = false;
                 }
-                RobotLog.dd(TAG,numButtonPresses+" press checkpoint 2: " + hardware.mag.currentState.toString());
+                RobotLog.dd(TAG,numButtonPresses+" press checkpoint 3: " + hardware.mag.currentState.toString());
                 if(hardware.mag.currentState == Mag.State.COLLECT){
                     hardware.mag.setRingPusherResting();
-                    RobotLog.dd(TAG,numButtonPresses+" press checkpoint 3: " + hardware.mag.currentState.toString());
+                    RobotLog.dd(TAG,numButtonPresses+" press checkpoint 4: " + hardware.mag.currentState.toString());
                 }
                 else{
                     sleeep(500);
                     hardware.shooter.shooterVeloPID.speedyRecoveryOn = false;
                     hardware.mag.pushInRings();
-                    RobotLog.dd(TAG,numButtonPresses+" press checkpoint 4 time: " + hardware.time.milliseconds());
+                    RobotLog.dd(TAG,numButtonPresses+" press checkpoint 5 time: " + hardware.time.milliseconds());
                     sleeep(500);
                     hardware.shooter.shooterVeloPID.speedyRecoveryOn = true;
                     hardware.mag.setRingPusherResting();
-                    RobotLog.dd(TAG,numButtonPresses+" press checkpoint 5 time: " + hardware.time.milliseconds());
+                    RobotLog.dd(TAG,numButtonPresses+" press checkpoint 6 time: " + hardware.time.milliseconds());
                     sleeep(250);
                     if(hardware.mag.currentState == Mag.State.BOTTOM){
                         hardware.mag.updateStateAndSetPosition();
