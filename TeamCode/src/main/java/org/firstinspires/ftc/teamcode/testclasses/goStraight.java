812c174b44d7f11940badae99f78e391175f6bf4
thomas <thomas11950@gmail.com>
2020-11-25 22:58:05 -0800

    goStraightTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testclasses/goStraight.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.testclasses;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Auto.AutoMethods;
+import org.firstinspires.ftc.teamcode.hardware.Hardware;
+import org.firstinspires.ftc.teamcode.hardware.HardwareThreadInterface;
+
+@Autonomous(name = "goStraightTest", group="auto")
+public class goStraight extends AutoMethods {
+    public void runOpMode(){
+        Hardware hardware = new Hardware(hardwareMap,telemetry);
+        HardwareThreadInterface hardwareThreadInterface = new HardwareThreadInterface(hardware,this);
+        waitForStart();
+        hardware.updatePID=false;
+        hardwareThreadInterface.start();
+        goStraightEncoder(-1,-15,hardware);
+    }
+}
