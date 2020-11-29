5ce213b6275767b8e723aef3be8a4c1a2979e2b1
thomas <thomas11950@gmail.com>
2020-11-28 16:09:18 -0800

    many update PIDs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Multithreads/ShootPowershot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/Multithreads/ShootPowershot.java
@@ -36,20 +36,24 @@ public class ShootPowershot extends Thread{
         }
     }
     public void run(){
+        hardware.turret.updatePID = true;
         hardware.mag.feedTopRing();
         hardware.mag.currentState = Mag.State.TOP;
         hardware.turret.updatePID = true;
         hardware.turret.turretPID.setState(MathFunctions.keepAngleWithin180Degrees(Math.toRadians(0)));
+        hardware.turret.updatePID = true;
         sleeep(1500);
         shootPowershot(hardware);
         telemetry.addLine("1st powershot");
         telemetry.update();
         hardware.turret.turretPID.setState(MathFunctions.keepAngleWithin180Degrees(Math.toRadians(-4)));
+        hardware.turret.updatePID = true;
         sleeep(1500);
         shootPowershot(hardware);
         telemetry.addLine("2nd powershot");
         telemetry.update();
         hardware.turret.turretPID.setState(MathFunctions.keepAngleWithin180Degrees(Math.toRadians(-9)));
+        hardware.turret.updatePID = true;
         sleeep(1500);
         shootPowershot(hardware);
         telemetry.addLine("3rd powershot");
